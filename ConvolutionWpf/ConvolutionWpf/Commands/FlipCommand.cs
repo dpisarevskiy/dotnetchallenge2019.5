f37c2ab02cd22d28ae13475a5597ecc8cc5df2f5
vladimir.galchenkov <volga.ndr@gmail.com>
2019-05-28 20:47:15 +0300

    Image Processing Blank

---
+++ ConvolutionWpf/ConvolutionWpf/Commands/FlipCommand.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media.Imaging;
+using Catel.MVVM;
+
+namespace ConvolutionWpf.Commands
+{
+    public class FlipCommand : Command
+    {
+        private readonly Func<WriteableBitmap> _imageFactory;
+
+        public event Action<WriteableBitmap> OnImageChanged;
+
+        public FlipCommand(Func<WriteableBitmap> imageFactory)
+            : base(() => { })
+        {
+            _imageFactory = imageFactory;
+        }
+
+        public void ExecuteCommand()
+        {
+            var image = _imageFactory();
+            if (image == null)
+                return;
+
+            var pixels = new byte[image.PixelHeight * image.BackBufferStride];
+            image.CopyPixels(pixels, image.BackBufferStride, 0);
+
+            var imageRes = new WriteableBitmap(2 * image.PixelWidth, image.PixelHeight, image.DpiX, image.DpiY, image.Format, image.Palette);
+            var resultPixels = new byte[2*imageRes.PixelHeight * imageRes.BackBufferStride];
+
+            //todo
+
+            imageRes.WritePixels(new Int32Rect(0, 0, imageRes.PixelWidth, imageRes.PixelHeight), resultPixels, imageRes.BackBufferStride, 0);
+
+            OnImageChanged?.Invoke(imageRes);
+        }
+
+        protected override void Execute(object parameter, bool ignoreCanExecuteCheck)
+        {
+            ExecuteCommand();
+        }
+    }
+}
