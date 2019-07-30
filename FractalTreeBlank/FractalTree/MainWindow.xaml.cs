51a57900d01fa2ee3ff936a2afe94349b486faf4
vladimir.galchenkov <volga.ndr@gmail.com>
2019-07-30 16:23:24 +0300

    FractalTree

---
+++ FractalTreeBlank/FractalTree/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace FractalTree
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly Brush _brush = Brushes.SteelBlue;
+
+        public MainWindow()
+        {
+            Loaded += (sender, args) => DrawFractal(new Point(400, 450), 200, Math.PI / 2);
+            InitializeComponent();
+        }
+
+        private void DrawFractal(Point start, double length, double angle)
+        {
+            var myLine = new Line
+            {
+                Stroke = _brush,
+                X1 = start.X,
+                Y1 = start.Y,
+                X2 = start.X + length * Math.Cos(angle),
+                Y2 = start.Y - length * Math.Sin(angle),
+                StrokeThickness = 1
+            };
+
+            canvas.Children.Add(myLine);
+
+            //Задержка в отрисовке для создания анимации
+            Sleep();
+        }
+
+        private void Sleep(int ms = 1)
+        {
+            Dispatcher.Invoke(DispatcherPriority.Background, new ThreadStart(delegate { }));
+            Thread.Sleep(ms);
+        }
+    }
+}
